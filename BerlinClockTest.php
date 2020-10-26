62151b6c6188aaa102e550783b7b518358541099
adrien-dedecker-ipl <adrien.dedecker@student.vinci.be>
2020-10-26 15:54:00 +0100

    test commit

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -16,4 +16,12 @@ class BerlinClockTest extends TestCase
 
         $this->assertEquals("[x][ ][ ][ ]",$actual);
     }
+
+    public function test_minute3_shouldReturnOn(){
+        $BerlinClock = new BerlinClock();
+
+        $actual = $BerlinClock->countTime(3);
+
+        $this->assertEquals("[x][x][x][ ]",$actual);
+    }
 }
