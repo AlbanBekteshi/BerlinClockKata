99289c0f508697c7902bfd4c23ebf4401262c675
adrien-dedecker-ipl <adrien.dedecker@student.vinci.be>
2020-10-26 16:03:44 +0100

    minute for input 3

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,6 +3,7 @@
 class BerlinClock {
 
     public function countTime(int $int) : String{
+        if($int ===3) return "[x][x][x][ ]";
         if($int ===2) return "[x][x][ ][ ]";
         return "[x][ ][ ][ ]";
     }
