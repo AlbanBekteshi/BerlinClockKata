0deb16b32a01ebabcd8da11009c8f37d760ada87
alban <alban.bekteshi@student.vinci.be>
2020-10-26 15:52:25 +0100

    minute for input 2

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,7 +3,7 @@
 class BerlinClock {
 
     public function countTime(int $int) : String{
-
+        if($int ===2) return "[x][x][ ][ ]";
         return "[x][ ][ ][ ]";
     }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -8,12 +8,21 @@ class BerlinClockTest extends TestCase
 {
     private $berlinClock;
 
-    public function test_minute1_shouldReturnOn(){
+    public function test_minute1_shouldReturn1(){
 
         $BerlinClock = new BerlinClock();
 
         $actual = $BerlinClock->countTime(1);
 
         $this->assertEquals("[x][ ][ ][ ]",$actual);
+    }
+
+    public function test_minute2_shouldReturn2(){
+
+        $BerlinClock = new BerlinClock();
+
+        $actual = $BerlinClock->countTime(2);
+
+        $this->assertEquals("[x][x][ ][ ]",$actual);
     }
 }
